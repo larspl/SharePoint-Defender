b4a640b3ae78849e380ee6d836254762ad1f0bf4
larspl <lars@platzdasch.de>
2017-10-23 12:57:10 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+SharePoint Defender Wiki
+
+Welcome to the SharePoint-Defender wiki!
+
+Collection for PowerShell Scripts to get a better Collaboration between SharePoint and Windows Defender or other AntiVirus Products
+
+feel free to use.. 
+feel free to update and help to grow
+
+sharing is caring .. so enjoy
+
+Cheers Lars Platzdasch 
+feel free to contact me..
---
+++ Sp2016WindowsDefenderExclude.ps1
@@ -0,0 +1,24 @@
+
+# based on blog.platzdasch.de 
+# we use this script to exclude folders on a sharepoint Search Server to solve the 
+# cpu issue with windows defender
+
+Add-MpPreference -ExclusionPath "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions"
+Add-MpPreference -ExclusionPath "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16"
+Add-MpPreference -ExclusionPath "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\Logs"
+Add-MpPreference -ExclusionPath "C:\Program Files\Microsoft Office Servers\16.0\Data\Office Server\Applications"
+Add-MpPreference -ExclusionPath "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files"
+Add-MpPreference -ExclusionPath "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Config"
+
+# just change the user Name of SearchService... in our case it's SP_Crawl
+Add-MpPreference -ExclusionPath "C:\Users\SP_Crawl\AppData\Local\Temp\WebTempDir"
+
+
+Add-MpPreference -ExclusionPath "C:\WINDOWS\System32\LogFiles"
+Add-MpPreference -ExclusionPath "C:\Windows\Syswow64\LogFiles"
+
+# just change the user Name of SearchService... in our case it's SP_Crawl
+Add-MpPreference -ExclusionPath "C:\Users\SP_Crawl\AppData\Local\Temp"
+Add-MpPreference -ExclusionPath "C:\Users\Default\AppData\Local\Temp"
+Add-MpPreference -ExclusionPath "C:\inetpub\wwwroot\wss\VirtualDirectories" 
+Add-MpPreference -ExclusionPath "C:\inetpub\temp\IIS Temporary Compressed Files" 
